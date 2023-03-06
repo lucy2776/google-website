3e2b00a97a761b635bba7fb10c15fcd93a222fb9
lucy2776 <lucy2776@naver.com>
2023-03-06 20:11:47 +0900

    final

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,10 @@
-const API_KEY = config.apikey;
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+    const API_KEY  = config.apikey;
     
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
